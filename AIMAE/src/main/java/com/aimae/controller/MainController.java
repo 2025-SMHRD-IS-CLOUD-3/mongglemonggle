6cf488c2b6334975432ec7fb3f479cc3993305a3
CHANHEE HAN <mh981219@naver.com>
2025-08-18 15:20:43 +0900

    update

--- AIMAE/src/main/java/com/aimae/controller/MainController.java
+++ AIMAE/src/main/java/com/aimae/controller/MainController.java
@@ -1,9 +1,20 @@
 package com.aimae.controller;
 
-public class MainController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MainController extends HttpServlet {
 	
-	public static void main(String[] args) {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+	
+	
 	}
 
 }
